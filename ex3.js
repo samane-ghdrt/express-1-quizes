d1ab9dc0fe5bf5e5d5e07ed128b3109cc9590d2f
samane ghodrati <samane.ghodrati@gmail.com>
2025-04-23 02:44:20 -0900

    add

--- ex3.js
+++ ex3.js
@@ -144,24 +144,15 @@ const products = [
 const express=require('express');
 const app=express();
 app.get('/products',(req,res)=>{
-  let productsNew=[];
-  let prductNew1=[];
+  let productsNew=[...products];
+  // let prductNew1=[];
   if(req.query.maxprice) {
- productsNew=products.filter(prduct=>prduct.price<Number(req.query.maxprice))
+    productsNew=productsNew.filter(prduct=>prduct.price<Number(req.query.maxprice))
   }
   if(req.query.limit){
-    if (productsNew.length!==0) {
-     prductNew1 =productsNew.slice(0,Number(req.query.limit))
-    } else {
-      prductNew1=products.slice(0,Number(req.query.limit))
-    }
+     productsNew=productsNew.slice(0,Number(req.query.limit))
   }
-  if (prductNew1) {
-    res.status(200).json(prductNew1)
-  } else {
-    res.status(200).json(products)
-    
-  }
+    res.status(200).json(productsNew)
 })
 app.listen('4000',(err)=>{
   if (err) {
